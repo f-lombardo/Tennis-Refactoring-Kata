adf137e95c9d4b172fc1d0842b36f70841a786e5
Franco Lombardo <f.lombardo69@gmail.com>
2014-11-10 23:56:16 +0100

    Generalize the idea of a describe function

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,83 +1,98 @@
-
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
+	private int player1Score = 0;
+	private int palyer2Score = 0;
+	private String player1Name;
+	private String player2Name;
 
-    public void wonPoint(String playerName) {
-        if (player1Name.equals(playerName)) {
-            m_score1 += 1;
-        } else if (player2Name.equals(playerName)) {
-            m_score2 += 1;
-        } else {
-        	throw new RuntimeException("Wrong player name " + playerName);
-        }
-    }
+	public TennisGame1(String player1Name, String player2Name) {
+		this.player1Name = player1Name;
+		this.player2Name = player2Name;
+	}
 
-    public String getScore() {
-        String score = "";
-        if (sameScore())
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (advantagesOrWinPhase())
-        {
-            int scoreDifference = m_score1-m_score2;
-            if (scoreDifference==1) score ="Advantage player1";
-            else if (scoreDifference ==-1) score ="Advantage player2";
-            else if (scoreDifference>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-           score = describe(m_score1) + "-" + describe(m_score2);
-        }
-        return score;
-    }
+	public void wonPoint(String playerName) {
+		if (player1Name.equals(playerName)) {
+			player1Score += 1;
+		} else if (player2Name.equals(playerName)) {
+			palyer2Score += 1;
+		} else {
+			throw new RuntimeException("Wrong player name " + playerName);
+		}
+	}
+
+	public String getScore() {
+		if (sameScore()) {
+			return describeTie();
+		} else if (advantages()) {
+			return describeAdvantages();
+		} else if (win()) {
+			return describeWin();
+		} else {
+			return describe(player1Score) + "-" + describe(palyer2Score);
+		}
+	}
+
+	private String describeWin() {
+		if (player1Score > palyer2Score) {
+			return "Win for player1";
+		} else {
+			return "Win for player2";
+		}
+	}
+
+	private String describeAdvantages() {
+		if (player1Score > palyer2Score) {
+			return "Advantage player1";
+		} else {
+			return "Advantage player2";
+		}
+	}
+
+	private boolean win() {
+		return advantagesOrWinPhase() && scoreDifference() >= 2;
+	}
+
+	private boolean advantages() {
+		return advantagesOrWinPhase() && scoreDifference() < 2;
+	}
+
+	private int scoreDifference() {
+		return Math.abs(player1Score - palyer2Score);
+	}
+
+	private String describeTie() {
+		switch (player1Score) {
+		case 0:
+			return "Love-All";
+		case 1:
+			return "Fifteen-All";
+		case 2:
+			return "Thirty-All";
+		default:
+			return "Deuce";
+		}
+	}
 
 	private String describe(int aScore) {
-		switch(aScore)
-		{
-		    case 0:
-		        return "Love";
-		    case 1:
-		        return "Fifteen";
-		    case 2:
-		    	return "Thirty";
-		    case 3:
-		    	return "Forty";
-		    default:
-		    	throw new RuntimeException("Program error: unknown score" + aScore);
+		switch (aScore) {
+		case 0:
+			return "Love";
+		case 1:
+			return "Fifteen";
+		case 2:
+			return "Thirty";
+		case 3:
+			return "Forty";
+		default:
+			throw new RuntimeException("Program error: unknown score " + aScore);
 		}
 	}
 
 	private boolean advantagesOrWinPhase() {
-		return m_score1>=4 || m_score2>=4;
+		return player1Score >= 4 || palyer2Score >= 4;
 	}
 
 	private boolean sameScore() {
-		return m_score1==m_score2;
+		return player1Score == palyer2Score;
 	}
 }
